05fe41354d892f868e18ebdfd988def8630f194b
GautFR <gautier.francois.pro@gmail.com>
2025-04-29 16:10:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lime_explainer_agent',
+    name='explainer_agent',
     version='0.1.0',
     author='Ton Nom',
     author_email='ton.email@example.com',
